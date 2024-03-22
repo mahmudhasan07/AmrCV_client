fe15abbf991397b08f9c0384f0e38e766d8ecbd6
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-22 23:47:56 +0600

    working on dashboard

--- src/Components/DashBoard/DashBoard.jsx
+++ src/Components/DashBoard/DashBoard.jsx
@@ -3,6 +3,13 @@ import { IoPerson } from "react-icons/io5";
 import { VscEye } from "react-icons/vsc";
 import { CiEdit } from "react-icons/ci";
 import { PiNoteFill } from "react-icons/pi";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+import { MdWorkHistory } from "react-icons/md";
+import { IoShareSocial } from "react-icons/io5";
+import { MdAnalytics } from "react-icons/md";
+
+
+
 
 
 
@@ -32,7 +39,7 @@ const DashBoard = () => {
             </div>
             <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
                 <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
+                    <BiSolidCategoryAlt className='text-3xl my-auto'></BiSolidCategoryAlt>
                     <h1>Skill Info</h1>
                 </div>
                 <div className='flex justify-around my-2'>
@@ -42,7 +49,7 @@ const DashBoard = () => {
             </div>
             <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
                 <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
+                    <MdWorkHistory className='text-3xl my-auto'></MdWorkHistory>
                     <h1>Work Experience</h1>
                 </div>
                 <div className='flex justify-around my-2'>
@@ -52,7 +59,7 @@ const DashBoard = () => {
             </div>
             <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
                 <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
+                    <IoShareSocial className='text-3xl my-auto'></IoShareSocial>
                     <h1>Social Presence</h1>
                 </div>
                 <div className='flex justify-around my-2'>
@@ -62,7 +69,7 @@ const DashBoard = () => {
             </div>
             <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
                 <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
+                    <MdAnalytics className='text-3xl my-auto'></MdAnalytics>
                     <h1>Analytics</h1>
                 </div>
                 <div className='flex justify-around my-2'>
