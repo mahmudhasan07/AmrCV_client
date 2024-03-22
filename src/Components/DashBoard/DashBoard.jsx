4b8ee7517d02e5fb2a5d212f99bc1a64ca952226
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-23 02:15:17 +0600

    complete profile section

--- src/Components/DashBoard/DashBoard.jsx
+++ src/Components/DashBoard/DashBoard.jsx
@@ -16,65 +16,73 @@ import { MdAnalytics } from "react-icons/md";
 
 const DashBoard = () => {
     return (
-        <section>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <IoPerson className='text-3xl my-auto'></IoPerson>
-                    <h1>Personal Info</h1>
+        <section className='text-black  '>
+            <div className='flex justify-center my-5 gap-10'>
+                <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
+                    <div className='flex gap-2 text-2xl font-semibold justify-center my-5'>
+                        <IoPerson className='text-3xl my-auto'></IoPerson>
+                        <h1>Personal Info</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
-                </div>
-            </div>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
-                    <h1>Education Info</h1>
-                </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
-                </div>
-            </div>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <BiSolidCategoryAlt className='text-3xl my-auto'></BiSolidCategoryAlt>
-                    <h1>Skill Info</h1>
-                </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                <div className='bg-[#F6FFA1] w-72  p-3 rounded-xl border-2 border-black'>
+                    <div className='flex text-2xl gap-2 font-semibold justify-center my-5'>
+                        <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
+                        <h1>Education Info</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
             </div>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <MdWorkHistory className='text-3xl my-auto'></MdWorkHistory>
-                    <h1>Work Experience</h1>
+
+            <div className='flex justify-center my-5 gap-10'>
+                <div className='bg-[#F6FFA1] w-72  p-3 rounded-xl border-2 border-black'>
+                    <div className='flex text-2xl gap-2 font-semibold justify-center my-5'>
+                        <BiSolidCategoryAlt className='text-3xl my-auto'></BiSolidCategoryAlt>
+                        <h1>Skill Info</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                <div className='bg-[#F6FFA1] w-72  p-3 rounded-xl border-2 border-black'>
+                    <div className='flex text-2xl gap-2 font-semibold justify-center my-5'>
+                        <MdWorkHistory className='text-3xl my-auto'></MdWorkHistory>
+                        <h1>Work Experience</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
             </div>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <IoShareSocial className='text-3xl my-auto'></IoShareSocial>
-                    <h1>Social Presence</h1>
+
+            <div className='flex justify-center my-5 gap-10'>
+                <div className='bg-[#F6FFA1] w-72  p-3 rounded-xl border-2 border-black'>
+                    <div className='flex text-2xl gap-2 font-semibold justify-center my-5'>
+                        <IoShareSocial className='text-3xl my-auto'></IoShareSocial>
+                        <h1>Social Presence</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
-                </div>
-            </div>
-            <div className='bg-[#F6FFA1] w-72 p-3 rounded-xl border-2 border-black'>
-                <div className='flex text-2xl font-semibold justify-center my-5'>
-                    <MdAnalytics className='text-3xl my-auto'></MdAnalytics>
-                    <h1>Analytics</h1>
-                </div>
-                <div className='flex justify-around my-2'>
-                    <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
-                    <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                <div className='bg-[#F6FFA1] w-72  p-3 rounded-xl border-2 border-black'>
+                    <div className='flex text-2xl gap-2 font-semibold justify-center my-5'>
+                        <MdAnalytics className='text-3xl my-auto'></MdAnalytics>
+                        <h1>Analytics</h1>
+                    </div>
+                    <div className='flex justify-around my-2'>
+                        <button className='btn border-2 border-black '><VscEye className='text-lg'></VscEye>View</button>
+                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                    </div>
                 </div>
             </div>
         </section>
