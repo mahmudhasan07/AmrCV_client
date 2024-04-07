4cd82f9ab2bb4e273142cec1c099bb8250411b49
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-04-08 03:44:50 +0600

    create image section

--- src/Components/DashBoard/BasicInfo/BasicInfo.jsx
+++ src/Components/DashBoard/BasicInfo/BasicInfo.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import noimage from "../../../../public/no-profile.png"
 
 const BasicInfo = () => {
+    const [image, setimage] = useState(noimage);
     return (
         <section>
             <h1>Enter Your Basic Information</h1>
@@ -10,13 +12,19 @@ const BasicInfo = () => {
                     <input type="text" className='w-72 rounded-2xl p-1 ' />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your Name</label>
+                    <label htmlFor="">Enter Your email</label>
                     <input type="text" className='w-72 rounded-2xl p-1 ' />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your Name</label>
+                    <label htmlFor="">Enter Your Address</label>
                     <input type="text" className='w-72 rounded-2xl p-1 ' />
                 </div>
+                <div>
+                    <label htmlFor="">Upload Your Image</label>
+                    <img src={image} className='' alt="" />
+                    <input type="text" className='w-72 rounded-2xl p-1 ' />
+                </div>
+
             </form>
         </section>
     );
