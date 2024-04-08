384f23c067acc0841015524cddf7ed98581a8600
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-04-08 16:54:59 +0600

    set modal for basic info

--- src/Components/DashBoard/BasicInfo/BasicInfo.jsx
+++ src/Components/DashBoard/BasicInfo/BasicInfo.jsx
@@ -4,25 +4,25 @@ import noimage from "../../../../public/no-profile.png"
 const BasicInfo = () => {
     const [image, setimage] = useState(noimage);
     return (
-        <section>
-            <h1>Enter Your Basic Information</h1>
-            <form action="">
+        <section className='p-5'>
+            <h1 className='text-3xl font-semibold my-5'>Enter Your Basic Information</h1>
+            <form action="" className='text-lg'>
                 <div>
-                    <label htmlFor="">Enter Your Name</label>
-                    <input type="text" className='w-72 rounded-2xl p-1 ' />
+                    <label htmlFor="">Enter Your Name</label> <br />
+                    <input type="text" className='w-96 border-2 rounded-2xl p-1 border-black ' /> <br />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your email</label>
-                    <input type="text" className='w-72 rounded-2xl p-1 ' />
+                    <label htmlFor="">Enter Your email</label> <br />
+                    <input type="text" className='w-80 border-2 rounded-2xl p-1  border-black' />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your Address</label>
-                    <input type="text" className='w-72 rounded-2xl p-1 ' />
+                    <label htmlFor="">Enter Your Address</label> <br />
+                    <input type="text" className='w-80 border-2 rounded-2xl p-1 border-black' />
                 </div>
                 <div>
                     <label htmlFor="">Upload Your Image</label>
-                    <img src={image} className='' alt="" />
-                    <input type="text" className='w-72 rounded-2xl p-1 ' />
+                    <img src={image} className='w-40 mx-auto' alt="" />
+                    <input type="file" className='w-80 rounded-2xl p-1 ' />
                 </div>
 
             </form>
--- src/Components/DashBoard/DashBoard.jsx
+++ src/Components/DashBoard/DashBoard.jsx
@@ -10,23 +10,28 @@ import { MdAnalytics } from "react-icons/md";
 import "./dashboard.css";
 import { TbPhotoFilled } from "react-icons/tb";
 import { FaDownload } from "react-icons/fa6";
+import BasicInfo from './BasicInfo/BasicInfo';
 
 
 const DashBoard = () => {
     return (
         <section className='text-black my-10 space-y-10'>
             <div className='space-y-4'>
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between space-y-2 mx-auto py-2 px-8 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between space-y-2 mx-auto py-2 px-8 rounded-xl border-2 border-black'>
                     <div className='flex my-auto gap-2 text-2xl font-semibold justify-center'>
                         <IoPerson className='text-3xl my-auto'></IoPerson>
                         <h1>Personal Info</h1>
                     </div>
                     <div className='flex justify-around gap-5'>
                         <button className=' btn border-2 border-black'><VscEye className='text-lg my-auto'></VscEye>View</button>
-                        <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
+                        <button onClick={() => document.getElementById("modal1").showModal()} className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
                     </div>
+                    {/* Modal Section */}
+                    <dialog id='modal1' className='rounded-2xl'>
+                        <BasicInfo></BasicInfo>
+                    </dialog>
                 </div>
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
                     <div className='flex my-auto text-2xl gap-2 font-semibold justify-center'>
                         <PiNoteFill className='text-3xl my-auto'></PiNoteFill>
                         <h1>Education Info</h1>
@@ -36,7 +41,7 @@ const DashBoard = () => {
                         <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
                     </div>
                 </div>
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8  p-3 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8  p-3 rounded-xl border-2 border-black'>
                     <div className='flex my-auto text-2xl gap-2 font-semibold justify-center '>
                         <BiSolidCategoryAlt className='text-3xl my-auto'></BiSolidCategoryAlt>
                         <h1>Skill Info</h1>
@@ -46,7 +51,7 @@ const DashBoard = () => {
                         <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
                     </div>
                 </div>
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
                     <div className='flex my-auto text-2xl gap-2 font-semibold justify-center'>
                         <MdWorkHistory className='text-3xl my-auto'></MdWorkHistory>
                         <h1>Work Experience</h1>
@@ -59,7 +64,7 @@ const DashBoard = () => {
 
 
 
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
                     <div className='flex my-auto text-2xl gap-2 font-semibold justify-center'>
                         <IoShareSocial className='text-3xl'></IoShareSocial>
                         <h1 className=''>Social Presence</h1>
@@ -69,7 +74,7 @@ const DashBoard = () => {
                         <button className='btn border-2 border-black '><CiEdit className='text-lg'></CiEdit>Edit</button>
                     </div>
                 </div>
-                <div   className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
+                <div className='dashboardCard bg-[#F6FFA1] w-1/2 flex justify-between mx-auto py-2 px-8 rounded-xl border-2 border-black'>
                     <div className='flex my-auto text-2xl gap-2 font-semibold justify-center'>
                         <MdAnalytics className='text-3xl my-auto'></MdAnalytics>
                         <h1>Analytics</h1>
