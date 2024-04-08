213183ff8ca9acb23dcaab8a2898be242a40acbf
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-04-09 03:35:11 +0600

    create educational section

--- src/Components/DashBoard/BasicInfo/BasicInfo.jsx
+++ src/Components/DashBoard/BasicInfo/BasicInfo.jsx
@@ -3,26 +3,49 @@ import noimage from "../../../../public/no-profile.png"
 
 const BasicInfo = () => {
     const [image, setimage] = useState(noimage);
+    const [hostImage, sethostImage] = useState();
+
+    function handleimage(e) {
+        e.preventDefault()
+        const image = e.target.files[0]
+        sethostImage(image)
+        const localHost = URL.createObjectURL(image)
+        setimage(localHost)
+    }
+
+    const handleBasicInfo=(e)=>{
+        e.preventDefault()
+        const from = e.target
+        // const 
+    }
+
     return (
-        <section className='p-5'>
+        <section className='px-16 py-5 '>
+            <div className='flex justify-end font-extrabold text-xl'>
+                <button onClick={() => document.getElementById("modal1").close()} className=''>X</button>
+            </div>
             <h1 className='text-3xl font-semibold my-5'>Enter Your Basic Information</h1>
-            <form action="" className='text-lg'>
+            <form onSubmit={handleBasicInfo} action="" className='text-lg'>
                 <div>
-                    <label htmlFor="">Enter Your Name</label> <br />
-                    <input type="text" className='w-96 border-2 rounded-2xl p-1 border-black ' /> <br />
+                    <label htmlFor="" className='font-bold'>Enter Your Name</label> <br />
+                    <input name='' type="text" className='w-96 border-2 rounded-2xl p-1 border-black ' /> <br />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your email</label> <br />
+                    <label htmlFor="" className='font-bold'>Enter Your email</label> <br />
                     <input type="text" className='w-96 border-2 rounded-2xl p-1  border-black' />
                 </div>
                 <div>
-                    <label htmlFor="">Enter Your Address</label> <br />
+                    <label htmlFor="" className='font-bold'>Enter Your Address</label> <br />
                     <input type="text" className='w-96 border-2 rounded-2xl p-1 border-black' />
                 </div>
                 <div>
-                    <label htmlFor="">Upload Your Image</label>
-                    <img src={image} className='w-40 mx-auto' alt="" />
-                    <input type="file" className='w-96 rounded-2xl p-1 ' />
+                    <label htmlFor="" className='font-bold'>What you are doing now?</label> <br />
+                    <input type="text" className='w-96 border-2 rounded-2xl p-1 border-black' />
+                </div>
+                <div>
+                    <label htmlFor="" className='font-bold'>Upload Your Image</label>
+                    <img src={image} className='w-48 h-48 rounded-full object-contain border-2 border-gray-700 mx-auto' alt="" />
+                    <input onChange={handleimage} type="file" className='w-96 rounded-2xl  p-1 ' />
                 </div>
 
             </form>
---
+++ src/Components/DashBoard/Education/Education.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <section>
+            <h1>Enter Your Educational Information</h1>
+            <div id='degree'>
+                <div>
+                    <label htmlFor="">Enter </label>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Education;
